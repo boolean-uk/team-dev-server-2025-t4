cc708a665e1bba2eeddafed7554cc17b0008abb4
Marcus0410 <marcus.ikdal@gmail.com>
2025-02-28 09:04:50 +0100

    implemented updateProfile

--- src/controllers/profile.js
+++ src/controllers/profile.js
@@ -1,5 +1,6 @@
-import { sendDataResponse } from '../utils/responses.js'
+import { sendDataResponse, sendMessageResponse } from '../utils/responses.js'
 import User from '../domain/user.js'
+import dbClient from '../utils/dbClient.js'
 
 export const createProfile = async (req, res) => {
   const paramId = parseInt(req.params.id)
@@ -13,4 +14,43 @@ export const createProfile = async (req, res) => {
   const createdProfile = await profile.createProfile(paramId)
 
   return sendDataResponse(res, 201, createdProfile)
+}
+
+export const updateProfile = async (req, res) => {
+  const paramId = parseInt(req.params.id)
+
+  try {
+    const user = await User.findById(paramId)
+
+    if (!user) {
+      return sendDataResponse(res, 404, { error: 'User not found' })
+    }
+
+    console.log(user)
+
+    if (!user.profile) {
+      return sendDataResponse(res, 404, {
+        error: 'User has no profile. Create one first'
+      })
+    }
+
+    const { firstName, lastName, bio, githubUrl, username, mobile } = req.body
+
+    const updatedProfile = await dbClient.profile.update({
+      where: { userId: paramId },
+      data: {
+        firstName: firstName || user.profile.firstName,
+        lastName: lastName || user.profile.lastName,
+        bio: bio ?? user.profile.bio,
+        githubUrl: githubUrl ?? user.profile.githubUrl,
+        username: username ?? user.profile.username,
+        mobile: mobile ?? user.profile.mobile
+      }
+    })
+
+    return sendDataResponse(res, 200, updatedProfile)
+  } catch (error) {
+    console.error(error)
+    return sendMessageResponse(res, 500, 'Unable to update profile')
+  }
 }
--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,9 +1,10 @@
 import { Router } from 'express'
-import { createProfile } from '../controllers/profile.js'
+import { createProfile, updateProfile } from '../controllers/profile.js'
 import { validateAuthentication } from '../middleware/auth.js'
 
 const router = Router()
 
 router.post('/:id', validateAuthentication, createProfile)
+router.patch('/:id', validateAuthentication, updateProfile)
 
 export default router
