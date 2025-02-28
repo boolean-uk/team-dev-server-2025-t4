95b89cdea4a9af369505e520f2bbf31c2135cd96
Marcus0410 <marcus.ikdal@gmail.com>
2025-02-28 09:16:56 +0100

    fixed validation for patch endpoint

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,10 +1,13 @@
 import { Router } from 'express'
 import { createProfile, updateProfile } from '../controllers/profile.js'
-import { validateAuthentication } from '../middleware/auth.js'
+import {
+  validateAuthentication,
+  validatePatchPermissionsForProfiles
+} from '../middleware/auth.js'
 
 const router = Router()
 
 router.post('/:id', validateAuthentication, createProfile)
-router.patch('/:id', validateAuthentication, updateProfile)
+router.patch('/:id', validatePatchPermissionsForProfiles, updateProfile)
 
 export default router
